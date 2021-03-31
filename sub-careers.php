17ad79b2ad642607243007684017f04eadc51b3b
mac <mac@macs-MacBook-Pro.local>
2021-03-31 12:11:33 +0200

    testlar

---
+++ sub-careers.php
@@ -0,0 +1,184 @@
+<?php
+include_once("header.php")
+?>
+
+
+<!--Shopping-->
+<section id="our-shop" class="career-mob">
+
+
+    <div class="container bg-light p-0 mt-5">
+
+
+
+        <div class="row">
+
+            <div class="col-md-9 col-6">
+                <h3 class=" pl-3 pt-2">
+                    <a href="#">المهن الحرة</a>
+                    <a href="#" class="main-color">/ طبيب</a>
+                </h3>
+            </div>
+
+            <div class="col-md-3 col-6">
+                <a href="#" class="button gradient-btn fz-22  float-right">أضف إعلانك</a>
+
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="container bg-light p-0 mt-3">
+        <div class="row">
+
+            <div class="col-md-9">
+
+                <form class="getin_form row" onsubmit="return false;">
+
+                    <div class="col-md-2 col-12">
+                        <h3 class=" pl-3 ml-1 pt-3">
+                            ابحث عن :
+                        </h3>
+                    </div>
+                    
+
+
+
+                    <div class="col-md-3 col-6 mt-2">
+                        <div class="form-group">
+                            <select class="form-control">
+                                <option value="اختر القسم" class="text-primary">اختر القسم</option>
+                                <option value="">طبيب</option>
+                                <option value="">محامي</option>
+                                <option value="">مهندس</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-6 mt-2">
+                        <div class="form-group">
+                            <select class="form-control">
+                                <option value="جميع المدن" class="text-primary">جميع المدن</option>
+                                <option value="">أكتوبر</option>
+                                <option value="">الفيوم</option>
+                                <option value="">كفرالشيخ</option>
+                            </select>
+                        </div>
+                    </div>
+
+                </form>
+
+            </div>
+
+            <div class="col-md-3 pr-5 pt-2 mt-1">
+                <p class="fz-22 float-right">
+                    مجموع النتائج : 4
+
+                </p>
+
+            </div>
+
+        </div>
+
+
+
+    </div>
+
+</section>
+<!--Shopping ends-->
+
+
+
+<section class="padding">
+    <div class="container">
+
+        <div class="row equal-shadow-team">
+            <!--item 1-->
+            <div class="col-lg-3 col-md-4 col-6 pb-4">
+                <a href="#">
+                <div class="team-box">
+                    <div class="image">
+                        <img src="img/team.jpg" alt="">
+                    </div>
+                    <div class="team-content">
+                        <h3 class="darkcolor">طبيب عام</h3>
+                        <ul>
+                            <li><span class="font-weight-bold">الخبرة: </span> 1 - 2 سنة</li>
+                            <li><span class="font-weight-bold">المدينة: </span> أكتوبر</li>
+                            <li><span class="font-weight-bold">تاريخ الإضافة: </span> 10/03/2021</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </a>
+            </div>
+            <!--item 2-->
+            <div class="col-lg-3 col-md-4 col-6 pb-4">
+                <a href="#">
+                <div class="team-box">
+                    <div class="image">
+                        <img src="img/team.jpg" alt="">
+                    </div>
+                    <div class="team-content">
+                        <h3 class="darkcolor">طبيب عام</h3>
+                        <ul>
+                            <li><span class="font-weight-bold">الخبرة: </span> 1 - 2 سنة</li>
+                            <li><span class="font-weight-bold">المدينة: </span> أكتوبر</li>
+                            <li><span class="font-weight-bold">تاريخ الإضافة: </span> 10/03/2021</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </a>
+            </div>
+            <!--item 3-->
+            <div class="col-lg-3 col-md-4 col-6 pb-4">
+                <a href="#">
+                <div class="team-box">
+                    <div class="image">
+                        <img src="img/team.jpg" alt="">
+                    </div>
+                    <div class="team-content">
+                        <h3 class="darkcolor">طبيب عام</h3>
+                        <ul>
+                            <li><span class="font-weight-bold">الخبرة: </span> 1 - 2 سنة</li>
+                            <li><span class="font-weight-bold">المدينة: </span> أكتوبر</li>
+                            <li><span class="font-weight-bold">تاريخ الإضافة: </span> 10/03/2021</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </a>
+            </div>
+            <!--item 4-->
+            <div class="col-lg-3 col-md-4 col-6 pb-4">
+                <a href="#">
+                <div class="team-box">
+                    <div class="image">
+                        <img src="img/team.jpg" alt="">
+                    </div>
+                    <div class="team-content">
+                        <h3 class="darkcolor">طبيب عام</h3>
+                        <ul>
+                            <li><span class="font-weight-bold">الخبرة: </span> 1 - 2 سنة</li>
+                            <li><span class="font-weight-bold">المدينة: </span> أكتوبر</li>
+                            <li><span class="font-weight-bold">تاريخ الإضافة: </span> 10/03/2021</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </a>
+            </div>
+            
+           
+        </div>
+    </div>
+</section>
+
+
+<!--Site Footer Here-->
+
+<?php
+include_once("footer.php")
+?>
